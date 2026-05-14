35ebaf19952633eac41eacec64b53edd1552adef
pa-vi <pavi12r7@gmail.com>
2026-05-14 22:10:45 +0530

    Create stcn.py

---
+++ Array/smaller-than-current-number/stcn.py
@@ -0,0 +1,18 @@
+# its my solution. This is brute force one , not optimal
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+
+        ans = []
+
+        for i in range(len(nums)):
+
+            count = 0
+
+            for j in range(len(nums)):
+
+                if nums[j] < nums[i]:
+                    count += 1
+
+            ans.append(count)
+
+        return ans
